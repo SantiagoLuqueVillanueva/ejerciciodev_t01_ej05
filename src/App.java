146ec6ffbf660e9d578ab6d0b9b11b0b8dafd839
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-09-29 17:58:26 +0200

    Colores añadidos

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    System.out.println("Lunes\tMartes\tMiérc\tJueves\tViernes");
+    System.out.println("=====\t======\t=====\t======\t=======");
+    System.out.print("\u001b[33mSIINF\t\u001b[32mPROGR\t\u001b[33mSIINF\t\u001b[31mIPE 1\t\u001b[36mBADAT\n"); System.out.print("\u001b[33mSIINF\t\u001b[32mPROGR\t\u001b[33mSIINF\t\u001b[31mIPE 1\t\u001b[36mBADAT\n");
+    System.out.print("\u001b[34mLMSGI\t\u001b[32mPROGR\t\u001b[31mIPE 1\t\u001b[32mPROGR\t\u001b[33mSIINF\n");
+    System.out.print("\u001b[36mBADAT\t\u001b[36mBADAT\t\u001b[32mPROGR\t\u001b[32mPROGR\t\u001b[37mENDES\n");
+    System.out.print("\u001b[36mBADAT\t\u001b[35mSASP\t\u001b[32mPROGR\t\u001b[34mLMSGI\t\u001b[37mENDES\n");
     }
 }
